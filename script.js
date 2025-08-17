0bb50a914a8295b8d5d5491116126526c2d4f714
anuragsoni5643 <sonianurag506@gmail.com>
2025-08-17 16:09:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function toFahrenheit(celsius) {
  // Write your code here
-	const feh = celsius*(9/5)+32; 
+	let feh = celsius*(9/5)+32; 
 	let realFeh=feh.toFixed(2);
-	console.log(realFeh);
-	  
+	
+	    return realFeh;
 }
  
 // Do not change the code below
